024aa2a823ecd634926b13078dca04fb498deb29
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-05-20 00:23:31 +0000

    fazendo o pareamento do cliente

--- funcoes.c
+++ funcoes.c
@@ -5,3 +5,11 @@
 #include <time.h>
 //define a quantidade maxima do array
 #define QTD_CLIENTES 65
+//paramentros do cliente
+typedef struct {
+    char nome[100];
+    char cpf[10];
+    char tipoConta[10];
+    float saldo;
+    char senha[20];
+} Cliente;
