ee630d141ba8ec7021cc8f67b4c059e907b4105c
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-05-20 00:22:52 +0000

    comecando com os primeiros passos das minhas funcoes

---
+++ funcoes.c
@@ -0,0 +1,7 @@
+//o arquivo programa que esta aberto eh a compilacao(./programa no terminal)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+//define a quantidade maxima do array
+#define QTD_CLIENTES 65
