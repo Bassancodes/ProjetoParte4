0285958e45dfdc76e809ea276e1e825b74cea780
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-05-20 00:21:11 +0000

    mostrando na tela o meu menu

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include "funcoes.h"
 //o menu tem como funcao printar todas as opcoes e logo em seguida dependendo da escolha do usuario aplicar e colocar na tela a funcao desejada pelo cliente, uma funcao simples igual fizemos no banco de phyton porem a base para o banco funcionar 
-//meu listar cliente foi incrementado na funcao 7
+int main(){
+  int e;
+  printf("\nBem vindo ao Bancon");
+  printf("1 - novo cliente\n");
+  printf("2 - Apaga cliente\n");
+  printf("3 - listar cliente\n");
+  printf("4 - debito\n");
+  printf("5 - deposito\n");
+  printf("6 - extrato\n");
+  printf("7 - transferência entre contas\n");
+  printf("0 - sair\n");
+  scanf("%d", &e);
