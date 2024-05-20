b71b2c2c55bc410aeda7a31ffa60fe07e83800f9
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-05-20 00:16:13 +0000

    adicionando os primeiros passos para a construcao da main

---
+++ main.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "funcoes.h"
+//o menu tem como funcao printar todas as opcoes e logo em seguida dependendo da escolha do usuario aplicar e colocar na tela a funcao desejada pelo cliente, uma funcao simples igual fizemos no banco de phyton porem a base para o banco funcionar 
+//meu listar cliente foi incrementado na funcao 7
