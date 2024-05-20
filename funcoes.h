de9b8245e12ed81b9d4b068a3623e9b8a9f28fba
henrique bassan rebechi <145149804+Bassancodes@users.noreply.github.com>
2024-05-20 00:33:45 +0000

    dando funcionalidade a todas as funcoes

--- funcoes.h
+++ funcoes.h
@@ -7,3 +7,14 @@ typedef struct {
     float saldo;
     char senha[20];
 } Cliente;
+void novo();
+void debitocomsenha();
+void deposito();
+void extrato();
+void transferencia();
+void sair();
+void listarcliente();
+void apagacliente();
+void salvarClientes();
+void debitoapossenha(Cliente *cliente, double valor);
+#endif
